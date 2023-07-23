18b20e47756a6a7deaa2ff306e1bfba1275eebd5
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-07-23 19:18:02 +0530

    [yuvanthika] Add :created

--- OOPsPractice/Program.cs
+++ OOPsPractice/Program.cs
@@ -1,41 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace PracticeProblems
 {
-    public class Frequency
+    class Program
     {
-        public void CountFrequency()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Enter the limit of array:");
-            int size = Convert.ToInt32(Console.ReadLine());
-            int[] array = new int[size];
-            Console.WriteLine("Enter the elements:");
-            for (int i = 0; i < array.Length; i++)
+            bool flag = true;
+            Console.WriteLine("Practice Problems:");
+            Console.WriteLine("Choose your option:\n 1.Number of Duplicates\n 2.Unique elements\n 3.Frequency\n 4.MinMaxElements\n 5.Exit");
+            while (flag)
             {
-                array[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            var dict = new Dictionary<int, int>();
-            foreach (int value in array)
-            {
-                if (dict.ContainsKey(value))
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
                 {
-                    dict[value]++;
-                }
-                else
-                {
-                    dict[value] = 1;
+                    case 1:
+                        NumberOfDuplicates duplicates = new NumberOfDuplicates();
+                        duplicates.NoOfDuplicates();
+                        break;
+                    case 2:
+                        UniqueElements unique = new UniqueElements();
+                        unique.PrintUniqueElements();
+                        break;
+                    case 3:
+                        Frequency frequency = new Frequency();
+                        frequency.CountFrequency();
+                        break;
+                    case 4:
+                        MinMaxElements minMaxElements = new MinMaxElements();
+                        minMaxElements.FindMaxMinElement();
+                        break;
+                    case 5:
+                        flag = false;
+                        break;
+
+                    default:
+                        break;
                 }
             }
-            Console.WriteLine("Frequency of each element:");
-            foreach (var data in dict)
-            {
-                Console.WriteLine(data.Key + " appears " + data.Value + " times.");
-            }
+
         }
     }
-}
 }
