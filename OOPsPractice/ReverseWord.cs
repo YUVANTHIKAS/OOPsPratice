c0d15e9a54855a08c7755bbe54e0e41ac21a1214
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-07-23 20:09:56 +0530

    [yuvanthika] Add:created

--- OOPsPractice/ReverseWord.cs
+++ OOPsPractice/ReverseWord.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOPsPractice
+namespace PracticeProblems
 {
-    internal class ReverseWord
+    public class ReverseWord
     {
+        public void PrintReverseEachWord()
+        {
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] charArray = words[i].ToCharArray();
+                Array.Reverse(charArray);
+                words[i] = new string(charArray);
+            }
+            string reversedString = string.Join(" ", words);
+            Console.WriteLine("Reversed sentence:");
+            Console.WriteLine(reversedString);
+        }
     }
 }
